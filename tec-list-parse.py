9f0367755b8197864777e035da06f2f67b50ecfc
Igor Vuk <parcijala@gmail.com>
2013-10-07 23:28:04 +0200

    Added basic error checking, abstracted the downloading part for both functions/files, removed modeline

--- tec-list-parse.py
+++ tec-list-parse.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
-# vim: set ts=4 expandtab:
 
 from xml.dom import minidom
 import urllib2
 import datetime
+
+
+def getDataFile(Url, FileName):
+    """
+    Generic function for opening an URL and saving its content to file
+    """
+    try:
+        DataFile = urllib2.urlopen(Url)
+    except urllib2.URLError, e:
+        print "Got URLError from urllib2, reason: %s" % e.reason
+    else:
+        Output = open(FileName, 'wb')
+        Output.write(DataFile.read())
+        Output.close()
 
 
 def getHNBFile():
@@ -13,59 +26,57 @@ def getHNBFile():
     now = datetime.datetime.now()
     HnbUrl = "http://www.hnb.hr/tecajn/f%s.dat" % (now.strftime("%d%m%y"))
     HnbFileName = "f%s.dat" % (now.strftime("%d%m%y"))
-    DataFile = urllib2.urlopen(HnbUrl)
-    Output = open(HnbFileName, 'wb')
-    Output.write(DataFile.read())
-    Output.close()
+    getDataFile(HnbUrl, HnbFileName)
+    getHNBData(HnbFileName)
 
 
-def getDataFile(param):
+def getPBZDataFile():
     """
     Get the required data file from PBZ
     """
-    DataFile = urllib2.urlopen(param)
-    Output = open('PBZteclist.xml', 'wb')
-    Output.write(DataFile.read())
-    Output.close()
+    PBZUrl = 'http://www.pbz.hr/Downloads/PBZteclist.xml'
+    getDataFile(PBZUrl, 'PBZteclist.xml')
+    getPBZData('PBZteclist.xml')
 
 
-def getHNBData():
+def getHNBData(FileName):
     """
     Print out data from HNB
     """
-    now = datetime.datetime.now()
-    HnbFileName = "f%s.dat" % (now.strftime("%d%m%y"))
-
     print "--- HNB ---"
-    HnbFile = open(HnbFileName, "r")
-    HeaderLine = HnbFile.readline()
-    for elem in HnbFile:
-        elem = elem.strip()
-        column = elem.split()
-        print "%s %s" % (str(column[0])[3:6], column[2])
-    HnbFile.close()
+    try:
+        HnbFile = open(FileName, "r")
+    except IOError, e:
+        print "Got IOError, '%s: %s'" % (e.errno, e.strerror)
+    else:
+        HeaderLine = HnbFile.readline()
+        for elem in HnbFile:
+            elem = elem.strip()
+            column = elem.split()
+            print "%s %s" % (str(column[0])[3:6], column[2])
+        HnbFile.close()
 
 
-def getData(param):
+def getPBZData(FileName):
     """
     Print out data from PBZ
     """
-    doc = minidom.parse(param)
-    currencies = doc.getElementsByTagName("Currency")
-
-    print "--- PBZ ---"
-    for elem in currencies:
-        currname = elem.getElementsByTagName('Name')
-        currval = elem.getElementsByTagName('MeanRate')
-        for elem in currname:
-            ValName = elem.childNodes[0].nodeValue
-            for elem in currval:
-                ValMeanRate = elem.childNodes[0].nodeValue
-                print "%s %s" % (ValName, ValMeanRate)
+    print "--- PBZ ---\nName\tMean Rate"
+    try:
+        doc = minidom.parse(FileName)
+    except IOError, e:
+        print "Got IOError, '%s: %s'" % (e.errno, e.strerror)
+    else:
+        currencies = doc.getElementsByTagName("Currency")
+        for elem in currencies:
+            currname = elem.getElementsByTagName('Name')
+            currval = elem.getElementsByTagName('MeanRate')
+            for elem in currname:
+                ValName = elem.childNodes[0].nodeValue
+                for elem in currval:
+                    ValMeanRate = elem.childNodes[0].nodeValue
+                    print "%s\t%s" % (ValName, ValMeanRate)
 
 if __name__ == "__main__":
-    SourceUrl = 'http://www.pbz.hr/Downloads/PBZteclist.xml'
-    getDataFile(SourceUrl)
-    getData('PBZteclist.xml')
+    getPBZDataFile()
     getHNBFile()
-    getHNBData()
