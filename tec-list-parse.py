efb9bb443e9bd1451afd2aeaea0365b8f85ee8b6
lumatijev <lumatijev@gmail.com>
2015-04-29 11:46:32 +0200

    Added support for type argument which specifies currency rate type, fixed ERSTE incorrect rate bug

--- tec-list-parse.py
+++ tec-list-parse.py
@@ -8,16 +8,26 @@ from urllib.request import urlopen
 from xml.dom import minidom
 
 
-def get_hnb_data(currency):
+def get_hnb_data(currency, rate_type):
     '''
     Get the required data from HNB's website
     Print out the data
     '''
+    if rate_type.lower() == 'buy':
+        hnb_rate_type = 1
+        hnb_rate_title = 'Buy'
+    elif rate_type.lower() == 'sell':
+        hnb_rate_type = 3
+        hnb_rate_title = 'Sell'
+    else:
+        hnb_rate_type = 2
+        hnb_rate_title = 'Mean'
+
     now = datetime.now()
     hnb_file_name = 'f{}.dat'.format(now.strftime('%d%m%y'))
     hnb_url = 'http://www.hnb.hr/tecajn/{}'.format(hnb_file_name)
 
-    print('--- HNB ---\nName\tMean Rate')
+    print('--- HNB ---\nName\t{} Rate'.format(hnb_rate_title))
 
     with urlopen(hnb_url) as Url:
         # Skip the header line
@@ -26,19 +36,31 @@ def get_hnb_data(currency):
             column = elem.strip().decode('utf-8').split()
 
             if currency == 'all':
-                print('{}\t{}'.format(str(column[0])[3:6], column[2]))
+                print('{}\t{}'.format(str(column[0])[3:6],
+                                      column[hnb_rate_type]))
             elif currency.lower() == str(column[0])[3:6].lower():
-                print('{}\t{}'.format(str(column[0])[3:6], column[2]))
+                print('{}\t{}'.format(str(column[0])[3:6],
+                                      column[hnb_rate_type]))
 
 
-def get_pbz_data(currency):
+def get_pbz_data(currency, rate_type):
     '''
     Get the required data from PBZ's website
     Print out the data
     '''
+    if rate_type.lower() == 'buy':
+        pbz_rate_type = 'BuyRateForeign'
+        pbz_rate_title = 'Buy'
+    elif rate_type.lower() == 'sell':
+        pbz_rate_type = 'SellRateForeign'
+        pbz_rate_title = 'Sell'
+    else:
+        pbz_rate_type = 'MeanRate'
+        pbz_rate_title = 'Mean'
+
     pbz_url = 'http://www.pbz.hr/Downloads/PBZteclist.xml'
 
-    print('--- PBZ ---\nName\tMean Rate')
+    print('--- PBZ ---\nName\t{} Rate'.format(pbz_rate_title))
 
     with urlopen(pbz_url) as Url:
         doc = minidom.parse(Url)
@@ -47,14 +69,14 @@ def get_pbz_data(currency):
         currency_name = [elem.getElementsByTagName('Name')[0].firstChild.data
                          for elem in currencies]
         currency_value = \
-            [elem.getElementsByTagName('MeanRate')[0].firstChild.data
+            [elem.getElementsByTagName(pbz_rate_type)[0].firstChild.data
              for elem in currencies]
 
         '''
         # This is neater, but uses ordinary dict()
         currency_list = \
             {elem.getElementsByTagName('Name')[0].firstChild.data:
-             elem.getElementsByTagName('MeanRate')[0].firstChild.data
+             elem.getElementsByTagName(pbz_rate_type)[0].firstChild.data
              for elem in currencies}
         for name, value in currency_list.items():
             print('{}\t{}'.format(name, value))
@@ -69,24 +91,34 @@ def get_pbz_data(currency):
                 print('{}\t{}'.format(name, value))
 
 
-def get_erste_data(currency):
+def get_erste_data(currency, rate_type):
     '''
     Get the required data from Erste's website
     Print out the data
     '''
+    if rate_type.lower() == 'buy':
+        erste_rate_type = 't1'
+        erste_rate_title = 'Buy'
+    elif rate_type.lower() == 'sell':
+        erste_rate_type = 't3'
+        erste_rate_title = 'Sell'
+    else:
+        erste_rate_type = 't2'
+        erste_rate_title = 'Mean'
+
     now = datetime.now()
     erste_file_name = 'TL_{}.xml'.format(now.strftime('%Y%m%d'))
     erste_url = 'http://local.erstebank.hr/alati/SaveAsXML.aspx?ime={}'.format(
         erste_file_name)
 
-    print('--- ERSTE ---\nName\tMean Rate')
+    print('--- ERSTE ---\nName\t{} Rate'.format(erste_rate_title))
 
     with urlopen(erste_url) as Url:
         doc = minidom.parse(Url)
         currencies = doc.getElementsByTagName('valuta')
         currency_name = [elem.getElementsByTagName('opis')[0].firstChild.data
                          for elem in currencies]
-        currency_value = [elem.getElementsByTagName('t3')[0].firstChild.data
+        currency_value = [elem.getElementsByTagName(erste_rate_type)[0].firstChild.data
                           for elem in currencies]
 
     currency_list = OrderedDict(zip(currency_name, currency_value))
@@ -109,6 +141,8 @@ def doit():
             currencies', action='store_true', dest='all')
     parser.add_argument('-c', '--currency', help='Set the currency for which \
             the value is shown', type=str, dest='currency')
+    parser.add_argument('-t', '--type', help='Set the currency rate type for \
+            which the value is shown', type=str, dest='rate_type', default='mean')
     parser.add_argument('-s' '--source', help='Set the source from which the \
             data is retrieved', type=str, dest='source')
 
@@ -118,27 +152,27 @@ def doit():
         parser.print_help()
 
     if args.all and not args.source:
-        get_pbz_data('all')
-        get_erste_data('all')
-        get_hnb_data('all')
+        get_pbz_data('all', args.rate_type)
+        get_erste_data('all', args.rate_type)
+        get_hnb_data('all', args.rate_type)
     elif args.all and args.source:
         if args.source.lower() == 'HNB'.lower():
-            get_hnb_data('all')
+            get_hnb_data('all', args.rate_type)
         if args.source.lower() == 'PBZ'.lower():
-            get_pbz_data('all')
+            get_pbz_data('all', args.rate_type)
         if args.source.lower() == 'ERSTE'.lower():
-            get_erste_data('all')
+            get_erste_data('all', args.rate_type)
     elif args.currency and not args.source:
-        get_pbz_data(args.currency)
-        get_erste_data(args.currency)
-        get_hnb_data(args.currency)
+        get_pbz_data(args.currency, args.rate_type)
+        get_erste_data(args.currency, args.rate_type)
+        get_hnb_data(args.currency, args.rate_type)
     elif args.currency and args.source:
         if args.source.lower() == 'HNB'.lower():
-            get_hnb_data(args.currency)
+            get_hnb_data(args.currency, args.rate_type)
         if args.source.lower() == 'PBZ'.lower():
-            get_pbz_data(args.currency)
+            get_pbz_data(args.currency, args.rate_type)
         if args.source.lower() == 'ERSTE'.lower():
-            get_erste_data(args.currency)
+            get_erste_data(args.currency, args.rate_type)
 
 
 if __name__ == '__main__':
